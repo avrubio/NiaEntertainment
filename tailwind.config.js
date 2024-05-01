05e7219338fbe21624815d7ea4531e79c69a16e3
avrubio <ariadna.vanegas@gmail.com>
2024-05-01 09:17:22 -0400

    shopping cart final changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
   plugins: [require("daisyui", "@tailwindcss/aspect-ratio")],
   daisyui: {
     themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
+    darkTheme: "light", // name of one of the included themes for dark mode
     utils: true, // adds responsive and modifier utility classes
     prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
     logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
