2fff822326d48107bdf1b254c1bc6c802863c26a
avrubio <ariadna.vanegas@gmail.com>
2024-05-02 23:56:18 -0400

    removing base from vite.congfig.js

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,19 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    // outDir: "dist",
-    // assetsDir: "assets",
+    outDir: "dist",
+    assetsDir: "assets",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        aboutCollective: resolve(__dirname, "aboutCollective/index.html"),
-        aboutNiaEnt: resolve(__dirname, "aboutNiaEnt/index.html"),
-        aboutNiaExtension: resolve(__dirname, "aboutNiaExtension/index.html"),
-        shop: resolve(__dirname, "shop/index.html"),
-        contact: resolve(__dirname, "contact/index.html"),
-        newReleases: resolve(__dirname, "newReleases/index.html"),
+        main: resolve(__dirname, "/index.html"),
+        aboutCollective: resolve(__dirname, "/aboutCollective/index.html"),
+        aboutNiaEnt: resolve(__dirname, "/aboutNiaEnt/index.html"),
+        aboutNiaExtension: resolve(__dirname, "/aboutNiaExtension/index.html"),
+        shop: resolve(__dirname, "/shop/index.html"),
+        contact: resolve(__dirname, "/contact/index.html"),
+        newReleases: resolve(__dirname, ".newReleases/index.html"),
       },
     },
   },
-  base: "/",
+  // base: "/",
 });
