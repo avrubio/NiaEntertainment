5abda3a092bc3d5bf8a7d2f48999a67a4e8a4850
avrubio <ariadna.vanegas@gmail.com>
2024-05-03 15:28:35 -0400

    test remove resolve aboutNiaextension

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         aboutCollective: resolve(__dirname, "aboutCollective/index.html"),
         aboutNiaEnt: resolve(__dirname, "aboutNiaEnt/index.html"),
-        aboutNiaExtension: resolve(__dirname, "aboutNiaExtension/index.html"),
+        // aboutNiaExtension: resolve(__dirname, "aboutNiaExtension/index.html"),
         shop: resolve(__dirname, "shop/index.html"),
         contact: resolve(__dirname, "contact/index.html"),
         newReleases: resolve(__dirname, "newReleases/index.html"),
