7a5ffb475ca253374d5aedfaa130bb2da3e8db7e
avrubio <ariadna.vanegas@gmail.com>
2024-05-03 15:27:19 -0400

    testing vite.config.ts

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,15 @@ export default defineConfig({
     assetsDir: "assets",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "/index.html"),
-        aboutCollective: resolve(__dirname, "/aboutCollective/index.html"),
-        aboutNiaEnt: resolve(__dirname, "/aboutNiaEnt/index.html"),
-        aboutNiaExtension: resolve(__dirname, "/aboutNiaExtension/index.html"),
-        shop: resolve(__dirname, "/shop/index.html"),
-        contact: resolve(__dirname, "/contact/index.html"),
-        newReleases: resolve(__dirname, "/newReleases/index.html"),
+        main: resolve(__dirname, "index.html"),
+        aboutCollective: resolve(__dirname, "aboutCollective/index.html"),
+        aboutNiaEnt: resolve(__dirname, "aboutNiaEnt/index.html"),
+        aboutNiaExtension: resolve(__dirname, "aboutNiaExtension/index.html"),
+        shop: resolve(__dirname, "shop/index.html"),
+        contact: resolve(__dirname, "contact/index.html"),
+        newReleases: resolve(__dirname, "newReleases/index.html"),
       },
     },
   },
-  // base: "/",
+  base: "/",
 });
